ea875a9dfd9dda65a16550658c01ca0c56185d27
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2024-10-06 16:51:57 -0700

    Replace format instructions with partial prompt

--- prompts/zero_shot.py
+++ prompts/zero_shot.py
@@ -1,5 +1,8 @@
 ZERO_SHOT_PROMPT = """
 Given the following topic {input}, generate a quiz with a randon number of questions between 1 to 5.
 
-{format_instructions}
+The output should be formatted as a JSON instance that conforms to the JSON schema below.
+
+As an example, for the schema {{"properties": {{"foo": {{"title": "Foo", "description": "a list of strings", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}
+the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of the schema. The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted.
 """
