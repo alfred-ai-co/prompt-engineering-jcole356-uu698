f8cb666ec39df85b0c92c0903e0a0b5c8d8e15cc
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2024-10-07 20:01:09 -0700

    Update tree of thought prompt

--- prompts/tree_of_thought.py
+++ prompts/tree_of_thought.py
@@ -1,15 +1,19 @@
 TREE_OF_THOUGHT_PROMPT = """
-  Given the following topic {input}, generate a quiz with a randon number of questions between 1 to 5.
+  Given the following topic {input} generate a quiz.
 
-  If the quiz has more than three questions, make each question progressively more difficult.
+  The quiz should be assembled based on the following steps:
+
+  The quiz should have a random number of questions between 1 and 5.
+
+  If the quiz has more than 3 questions, make each question progressively more difficult.
 
   For all questions after the third, provide a hint in the question text
 
-  For each question return 6 multiple choice answers
+  For each question return at least 4 multiple choice answers
 
-  Return the questions and answers as a JSON object in the following format:
+  Return the questions and answers as a JSON object.
 
-  The top level property should be "questions" and the value should be an array
+  The top level property should be "questions" and the value should be an array.
 
   Each question in the array should be an object with the following properties: "question", "choices" and "answer"
 
